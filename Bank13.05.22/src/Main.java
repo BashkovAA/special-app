38841e115e0ed21d835b0042d0a9c0ac6b3fe005
Bashkov Alexander <bashkov1996ab@mail.ru>
2022-05-14 10:42:32 +0300

    title changed

--- Bank13.05.22/src/Main.java
+++ Bank13.05.22/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BankDetails details = new BankDetails();
         details.setBillNumber("3423543-604550-560--300");
-        Company company = new Company("Смарт-зкспресс", details);
+        Company company = new Company("Смарт-зкспресс-4444", details);
         // some code
         details.setBillNumber("3423543-604550-560--454");
         System.out.println(company);
